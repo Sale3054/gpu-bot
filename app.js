85f01cbc4b283187d9639c8eac82526277db7044
Clayton Skinner <cmskinner32@gmail.com>
2021-01-17 14:50:48 -0800

    Clicked on sign-in button

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ async function report (log) {
 	currentTime = new Date();
 	console.log(currentTime.toString().split('G')[0] + ': ' + log)
 }
-function click_button(x,y,page){
-  page.mouse.click(x,y);
+function click_signin(page){
+  page.click(".nav-complex-inner");
 }
 async function main(){
   const browser = await puppeteer.launch({"headless": false, "defaultViewport": null});
   const page = await browser.newPage();
 
   await page.goto('https://newegg.com');
-  click_button(500,500,page);
+  click_signin(page);
   //await browser.close();
 }
 
