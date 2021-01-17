8da9f199b95787c6cbd97af00892abed61f96bd0
Sam Leon <Sale3054@colorado.edu>
2021-01-17 16:17:06 -0700

    Got back to ground 0 with SOLID principles

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 const puppeteer = require('puppeteer')
 const config = require('./config.json')
 
-async function report (log) {
-	currentTime = new Date();
-	console.log(currentTime.toString().split('G')[0] + ': ' + log)
+class Logger
+{
+	constructor()
+	{
+		console.log("Creating new Logger instance");
+	}
+
+	log(message) {
+		this.currentTime = new Date();
+		console.log(this.currentTime.toString().split('G')[0] + ': ' + message);
+	}
 }
-function click_signin(page){
-  page.click(".nav-complex-inner");
+
+class Clicker
+{
+	constructor()
+	{
+		this.logger = new Logger();
+		this.logger.log("Creating new Clicker instance");
+	}
+
+	click_sign_in(page)
+	{
+		page.click(".nav-complex-inner");
+		page.waitForNavigation();
+	}
+
+	click_email_form(page)
+	{
+		page.click("#email");
+	}
 }
+
 async function main(){
   const browser = await puppeteer.launch({"headless": false, "defaultViewport": null});
   const page = await browser.newPage();
-
+	const clicker = new Clicker();
   await page.goto('https://newegg.com');
-  click_signin(page);
+
+  clicker.click_sign_in(page);
+	clicker.click_email_form(page);
   //await browser.close();
 }
 
