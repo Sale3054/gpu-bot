e5b3df555ed34bb6df290173207fe1646cb1457e
Clayton Skinner <symbioticpenguin@Claytons-MacBook-Pro.local>
2021-01-17 14:31:58 -0800

    We clicked on something yay

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ async function report (log) {
 	currentTime = new Date();
 	console.log(currentTime.toString().split('G')[0] + ': ' + log)
 }
-
-
-(async () => {
+function click_button(x,y,page){
+  page.mouse.click(x,y);
+}
+async function main(){
   const browser = await puppeteer.launch({"headless": false, "defaultViewport": null});
   const page = await browser.newPage();
 
   await page.goto('https://newegg.com');
-
+  click_button(500,500,page);
   //await browser.close();
-})();
+}
+
+main();
